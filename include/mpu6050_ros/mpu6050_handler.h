375f4b567753c2f6096d1f83a3b8621c11376624
dwindra <tandukion@hotmail.com>
2021-08-26 00:21:47 +0900

    Added complementary-filtered data as default data

--- include/mpu6050_ros/mpu6050_handler.h
+++ include/mpu6050_ros/mpu6050_handler.h
@@ -8,6 +8,9 @@
 #include "ros/ros.h"
 #include "MPU6050Pi.h"
 
+#define COMPLEMENTARY_FILTER_CONSTANT   0.98
+#define DEFAULT_RATE                    100
+
 class MPU6050Handler {
   private:
     // Main class members
@@ -17,6 +20,7 @@ class MPU6050Handler {
 
     // Data update thread
     ros::Rate update_rate_;
+    std::thread update_task_;
     std::thread update_dmp_task_;
 
   public:
@@ -25,18 +29,31 @@ class MPU6050Handler {
     float gyro_scale;
     uint8_t fifo_buffer[64];
 
+    float gyro[3];
+    float accel[3];
+    float rpy[3];       // roll pitch yaw
+    float rpy_comp[3];  // complementary roll pitch yaw
+
     /**
      * Constructor
      * 
      * @param nh {ros::NodeHandle}  ROS Node Handler
      * @param offsets{int16_t*}     List of MPU6050 offsets
+     * @param rate {int16_t}        frequency of data update
+     * @param dmp {bool}            flag to use DMP or not
      */
-    MPU6050Handler(ros::NodeHandle *nh, int16_t *offsets);
+    MPU6050Handler(ros::NodeHandle *nh, int16_t *offsets, int16_t rate=DEFAULT_RATE, bool dmp=false);
+
+    void UpdateData(int16_t rate);
 
     /**
      * Update DMP FIFO buffer
      */
     void UpdateDMP();
+
+    float ComplementaryFilter(float angle, float angle_comp) {
+      return COMPLEMENTARY_FILTER_CONSTANT * angle + (1- COMPLEMENTARY_FILTER_CONSTANT) * angle_comp;
+    }
 
 };
 
--- src/mpu6050_handler.cpp
+++ src/mpu6050_handler.cpp
@@ -4,11 +4,11 @@
 
 #include "mpu6050_ros/mpu6050_handler.h"
 
-MPU6050Handler::MPU6050Handler(ros::NodeHandle *nh, int16_t *offsets)
+MPU6050Handler::MPU6050Handler(ros::NodeHandle *nh, int16_t *offsets, int16_t rate, bool dmp)
   : // initialization list
   nh_(*nh),
   mpu_(MPU6050Pi()),
-  update_rate_(ros::Rate(100))
+  update_rate_(ros::Rate(rate))
 {
   // Set MPU6050 Offsets
   mpu_.SetOffset(offsets);
@@ -16,25 +16,62 @@ MPU6050Handler::MPU6050Handler(ros::NodeHandle *nh, int16_t *offsets)
   accel_scale = mpu_.GetAccelSensitivity();
   gyro_scale = mpu_.GetGyroSensitivity();
 
-  ROS_INFO("Initializing DMP..");
-  dev_status = mpu_.DMPInitalize();
-
-  // make sure it worked (returns 0 if so)
-  if (dev_status == 0) {
-      mpu_.SetDMPEnabled(true);
-      ROS_INFO("DMP enabled");
-      packet_size_ = mpu_.DMPGetFIFOPacketSize();
-  } else {
-      // ERROR!
-      // 1 = initial memory load failed
-      // 2 = DMP configuration updates failed
-      // (if it's going to break, usually the code will be 1)
-      ROS_ERROR("DMP Initialization failed.\n Error code: %s",dev_status);
+  if (!dmp) {
+    update_task_ = std::thread(&MPU6050Handler::UpdateData, this, rate);
   }
+  else {
+    ROS_INFO("Initializing DMP..");
+    dev_status = mpu_.DMPInitalize();
 
-  // // Start thread to update the data
-  ROS_INFO("DMP update thread started");
-  update_dmp_task_ = std::thread(&MPU6050Handler::UpdateDMP, this);
+    // make sure it worked (returns 0 if so)
+    if (dev_status == 0) {
+        mpu_.SetDMPEnabled(true);
+        ROS_INFO("DMP enabled");
+        packet_size_ = mpu_.DMPGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        ROS_ERROR("DMP Initialization failed.\n Error code: %s",dev_status);
+    }
+
+    // // Start thread to update the data
+    ROS_INFO("DMP update thread started");
+    update_dmp_task_ = std::thread(&MPU6050Handler::UpdateDMP, this);
+  }
+}
+
+void MPU6050Handler::UpdateData(int16_t rate) {
+  while (ros::ok()) {
+    // Get IMU data
+    mpu_.GetGyroFloat(&gyro[0], &gyro[1], &gyro[2]);
+    mpu_.GetAccelFloat(&accel[0], &accel[1], &accel[2]);
+
+    // Calculate the angles
+    // 1. Roll
+    //    From gyro. gyro data is in deg/s.
+    rpy[0] += gyro[0] * 1.0/rate;
+    //    From accel. atan or atan2f return is in rad
+    rpy_comp[0] = atan2f(accel[1], accel[2]) * 180/M_PI;
+    // Complementary Filter
+    rpy[0] = MPU6050Handler::ComplementaryFilter(rpy[0], rpy_comp[0]);
+
+    // 2. Pitch
+    //    From gyro. gyro data is in deg/s.
+    rpy[1] += gyro[1] * 1.0/rate;
+    //    From accel. atan or atan2f return is in rad
+    rpy_comp[1] = atan2f(accel[0], accel[2]) * 180/M_PI;
+    // Complementary Filter
+    rpy[1] = MPU6050Handler::ComplementaryFilter(rpy[1], rpy_comp[1]);
+
+    // 3. Yaw
+    //    From gyro. gyro data is in deg/s.
+    rpy[2] += gyro[2] * 1.0/rate;
+    
+    // Maintain refresh rate
+    update_rate_.sleep();
+  }
 }
 
 void MPU6050Handler::UpdateDMP() {
